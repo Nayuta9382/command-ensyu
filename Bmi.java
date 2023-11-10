92a4e39a863a82e53084e2244f99467cb64a05b4
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2023-11-10 11:21:29 +0900

    Main.javaをBmi.javaに修正

--- Bmi.java
+++ Bmi.java
@@ -1,7 +1,7 @@
 import javax.imageio.plugins.bmp.BMPImageWriteParam;
 import java.util.Scanner;
 
-public class Main {
+public class Bmi {
     public static void main(String args[] )  {
         Scanner sc = new Scanner(System.in);
         System.out.println("BMI計算サイト");
